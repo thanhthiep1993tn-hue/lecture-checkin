10bd04b3c1d02627bdc3a3e8b5cfb6b6bbcfe381
钟皓 <zhonghao@zhonghaodeMacBook-Air.local>
2026-04-29 22:14:04 +0800

    add smtp timeout

--- app.py
+++ app.py
@@ -269,7 +269,7 @@ Webull Event Team
     msg.set_content(plain_text)
     msg.add_alternative(html, subtype="html")
 
-    with smtplib.SMTP(SMTP_HOST, SMTP_PORT) as server:
+    with smtplib.SMTP(SMTP_HOST, SMTP_PORT, timeout=10) as server:
         server.starttls()
         server.login(SMTP_USER, SMTP_PASSWORD)
         server.send_message(msg)
